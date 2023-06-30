627cc84a49a767422d9d4fd4a125316fb3004f16
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-06-30 17:08:45 +0300

    some admin changes

--- models/Admin.js
+++ models/Admin.js
@@ -27,40 +27,6 @@ const AdminSchema = new mongoose.Schema({
     }
 });
 
-// Hash the password before saving it to the database
-AdminSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
 
-// Create a method to match the password
-AdminSchema.methods.matchPassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const Admin = mongoose.model('Admin', AdminSchema);
-
-// Check if a document with the default values already exists
-Admin.countDocuments({ name: 'Mohamed Bakr' }, (error, count) => {
-  if (error) console.log(error);
-
-  // If a document with the default values doesn't exist, create one
-  if (count === 0) {
-    const admin = new Admin({
-      name: 'Mohamed Bakr',
-      email: 'powerofmotion.eg@gmail.com',
-      username: 'POMAdmin',
-      password: '9%$ms%C?d_`xV9&'
-    });
-    admin.save((error, result) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(result);
-      }
-    });
-  }
-});
