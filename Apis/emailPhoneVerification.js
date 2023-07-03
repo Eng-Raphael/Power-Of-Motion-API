687a9105a71ac88e3520691c6176e227cb8b3b6f
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-07-03 17:30:41 +0300

    add more email validation

--- Apis/emailPhoneVerification.js
+++ Apis/emailPhoneVerification.js
@@ -1,8 +1,27 @@
 const validateEmail = async (email) => {
-    const response = await fetch(`https://emailvalidation.abstractapi.com/v1/?api_key=${process.env.abstract_api_email}&email=${email}`);
+    const response = await fetch(
+      `https://emailvalidation.abstractapi.com/v1/?api_key=${process.env.abstract_api_email}&email=${email}`
+    );
     const data = await response.json();
-    return data.is_valid_format.value && data.is_free_email.value && !data.is_disposable_email.value;
-}
+  
+    const isValidFormat = data.is_valid_format.value;
+    const isFreeEmail = data.is_free_email.value;
+    const isDisposableEmail = data.is_disposable_email.value;
+    const isRoleEmail = data.is_role_email.value;
+    const isCatchallEmail = data.is_catchall_email.value;
+    const isMXFound = data.is_mx_found.value;
+    const isSMTPValid = data.is_smtp_valid.value;
+  
+    return (
+      isValidFormat &&
+      isFreeEmail &&
+      !isDisposableEmail &&
+      !isRoleEmail &&
+      !isCatchallEmail &&
+      isMXFound &&
+      isSMTPValid
+    );
+};
 
 const validatePhoneNumber = async (phoneNumber) => {
     phoneNumber = encodeURIComponent('+2' + phoneNumber);
