c0f5c9c823438b4f233a1e6e52a3e23ef83473ff
rAboegila <rawan.moyasser.aboegila@gmail.com>
2023-07-05 06:29:56 +0300

    added getTickets By UserID route

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,53 +1,85 @@
-const express = require('express');
-const Ticket = require('../models/Ticket');
+const express = require("express");
+const Ticket = require("../models/Ticket");
 
-const { validateTicketCreate,validateTicketUpdate } = require('../validation/ticketValidation');
-const {protect , authorize ,authorizeMultiple} = require('../middleware/auth');
+const {
+  validateTicketCreate,
+  validateTicketUpdate,
+} = require("../validation/ticketValidation");
+const { protect, authorize, authorizeMultiple } = require("../middleware/auth");
 
-const advancedResults = require('../middleware/advancedResults');
+const advancedResults = require("../middleware/advancedResults");
 const router = express.Router();
 const {
-    createTicket,
-    getTickets,
-    getTicket,
-    updateTicket,
-    deleteTicket,
-    deleteTickets,
-    deleteTicketsByUser,
-    deleteTicketsByEvent,
-    getTicketsByEventAndUser,
-    getTicketsByEvent,
-    getTicketsByUser,
-}    = require('../controllers/ticket');
+  createTicket,
+  getTickets,
+  getTicket,
+  updateTicket,
+  deleteTicket,
+  deleteTickets,
+  deleteTicketsByUser,
+  deleteTicketsByEvent,
+  getTicketsByEventAndUser,
+  getTicketsByEvent,
+  getTicketsByUser,
+} = require("../controllers/ticket");
 
-router.route('/tickets')
-    .get(protect ,authorizeMultiple('admin','secretary') ,advancedResults(Ticket),getTickets)
+router
+  .route("/tickets")
+  .get(
+    protect,
+    authorizeMultiple("admin", "secretary"),
+    advancedResults(Ticket),
+    getTickets
+  );
 
-router.route('/ticket')
-    .post(protect,authorize('client'),validateTicketCreate,createTicket);
+router
+  .route("/ticket")
+  .post(protect, authorize("client"), validateTicketCreate, createTicket);
 
-router.route('/ticket/:id')
-    .get(protect ,authorizeMultiple('admin','secretary') ,getTicket)
-    .put(protect,authorize('client'),validateTicketUpdate,updateTicket)
-    .delete(protect,authorizeMultiple('admin','secretary'),deleteTicket);
+router
+  .route("/ticket/:id")
+  .get(protect, authorizeMultiple("admin", "secretary"), getTicket)
+  .put(protect, authorize("client"), validateTicketUpdate, updateTicket)
+  .delete(protect, authorizeMultiple("admin", "secretary"), deleteTicket);
 
-router.route('/tickets')
-    .delete(protect,authorizeMultiple('admin','secretary'),deleteTickets);
+router
+  .route("/tickets")
+  .delete(protect, authorizeMultiple("admin", "secretary"), deleteTickets);
 
+router
+  .route("/tickets/user/:userId")
+  .delete(
+    protect,
+    authorizeMultiple("admin", "secretary"),
+    deleteTicketsByUser
+  );
 
-router.route('/tickets/user/:userId')
-    .delete(protect,authorizeMultiple('admin','secretary'),deleteTicketsByUser);
+router
+  .route("/tickets/event/:eventId")
+  .delete(
+    protect,
+    authorizeMultiple("admin", "secretary"),
+    deleteTicketsByEvent
+  );
 
-router.route('/tickets/event/:eventId')
-    .delete(protect,authorizeMultiple('admin','secretary'),deleteTicketsByEvent);
+router
+  .route("/tickets/event/:eventId/user/:userId")
+  .get(
+    protect,
+    authorizeMultiple("admin", "secretary"),
+    getTicketsByEventAndUser
+  );
 
-router.route('/tickets/event/:eventId/user/:userId')
-    .get(protect,authorizeMultiple('admin','secretary'),getTicketsByEventAndUser);
+router
+  .route("/tickets/event/:eventId")
+  .get(protect, authorizeMultiple("admin", "secretary"), getTicketsByEvent);
 
-router.route('/tickets/event/:eventId')
-    .get(protect,authorizeMultiple('admin','secretary'),getTicketsByEvent);
+router
+  .route("/getTicketsByUser/user/:userId")
+  .get(protect, authorizeMultiple("admin", "secretary"), getTicketsByUser);
 
-router.route('/getTicketsByUser/user/:userId')
-    .get(protect,authorizeMultiple('admin','secretary'),getTicketsByUser);
+router
+  .route("/tickets/user/:userId")
+  .get(protect, authorize("client"), getTicketsByUser);
 
 module.exports = router;
